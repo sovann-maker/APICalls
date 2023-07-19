b9e36b446e25d2134baf4f39e573c0aae7219ec5
Sovann <chimsovann20@kit.edu.kh>
2023-07-20 00:21:40 +0700

    Initial Commit

---
+++ APICalls/APICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICallsApp.swift
+//  APICalls
+//
+//  Created by Macbook Pro on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct APICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
